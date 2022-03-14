63cbd8c29eed58a5c38deed9d43f3948926d9af5
2faceid <liorfeldman87@gmail.com>
2022-03-14 12:09:16 +0200

    14.3.11

--- actions.js
+++ actions.js
@@ -2,16 +2,15 @@ const express = require ('express');
 const router = express.Router();
 
 router.get('/greeting',(require,response) =>{
-
-
     return response.status(200).json({
         message : 'welcome to my node.js api'
     })
-
-
 });
+module.exports = router;
 
-
-
-
+router.get('/greetin',(require,response) =>{
+    return response.status(200).json({
+        message : 10*20
+    })
+});
 module.exports = router;
