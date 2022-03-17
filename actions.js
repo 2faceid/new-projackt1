e8b0524355d9610ed3f35b74fda288c4c2fc67cb
2faceid <liorfeldman87@gmail.com>
2022-03-17 13:13:14 +0200

    16.3.2022

--- actions.js
+++ actions.js
@@ -1,16 +1,31 @@
 const express = require ('express');
 const router = express.Router();
 
-router.get('/greeting',(require,response) =>{
-    return response.status(200).json({
-        message : 'welcome to my node.js api'
-    })
-});
-module.exports = router;
+//router.get('/greeting',(require,response) =>{
+//    return response.status(200).json({
+//        message : 'welcome to my node.js api'
+//    })
+//});
+//module.exports = router;
 
-router.get('/greetin',(require,response) =>{
-    return response.status(200).json({
-        message : 10*20
-    })
-});
+let users =[];
+//גם מקבלים וגם מחזרירים מידע
+router.post('/register',(request,response) => {    
+    // const email = request.body.email;
+    // const passwors = request.body.password;
+ 
+     const {email,password} = request.body;//דרך שונה להצגת המשתנים בתוך הגוף
+     
+     users.push({email:email, password:password});
+ 
+     //מחזיר את מה שרשמנו בתוך הגוף(מחזיר את המידע)
+     return response.status(200).json({
+        // email: `welcome - ${email} your passwors is ${passwors}`
+           message: users
+        }); 
+ });
+
+
+
+
 module.exports = router; 
